4f9e547404ed6f804882f413237d4ae77f0aa0bc
EstebanPechuan <esteban.pechuan@gmail.com>
2022-02-14 12:52:18 -0300

    ProgressBar - Primer form

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const stepsRegistro = document.querySelector('.pasos-registracion')
-const buttonsSteps = document.querySelectorAll('.buttonSiguiente')
+const buttonAtras = document.querySelectorAll('.buttonAtras')
+const buttonSiguiente = document.querySelectorAll('.buttonSiguiente')
 const mascaraGradient = document.querySelector('.mascara-gradient')
 
 let leftSteps = 0
 let leftMascara = 0
 
-buttonsSteps.forEach(button => {
+buttonSiguiente.forEach(button => {
     button.addEventListener('click', () => {
         leftSteps -= 100
         leftMascara += 20
         stepsRegistro.style.left = `${leftSteps}vw`
-        mascaraGradient.style.left = `${leftMascara}%`
+        mascaraGradient.style.left = `calc(${leftMascara}% - 40px)`
+    })
+})
+
+buttonAtras.forEach(button2 => {
+    button2.addEventListener('click', () => {
+        leftSteps += 100
+        leftMascara -= 20
+        stepsRegistro.style.left = `${leftSteps}vw`
+        mascaraGradient.style.left = `calc(${leftMascara}% - 40px)`
     })
 })
