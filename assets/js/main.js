cd97c56005fe47957632863f71d2cdb6cd9ae1ff
EstebanPechuan <esteban.pechuan@gmail.com>
2022-03-25 00:41:24 -0300

    Componentes

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,16 +24,103 @@ botonCards.forEach(item => {
 
 
 
+/* === Formulario === */
+
+// Datalist
+
+const inputs = document.querySelectorAll('.input-list')
+const datalistas = document.querySelectorAll('.datalista')
+
+inputs.forEach(item => {
+    item.addEventListener('click', () => {
+        datalistas.forEach(item2 => {
+            item2.style.display = 'block'
+            item2.style.display = 'none'
+        })                        
+    })
+})
 
 
 
+// Password show / hidden
+
+const iconosPass = document.querySelectorAll('.password-input')
+
+iconosPass.forEach(item => {
+    item.children[0].addEventListener('click', () => {
+        item.children[0].children[0].classList.toggle('d-none')
+        item.children[0].children[1].classList.toggle('d-none')
+
+        if (item.children[1].hasAttribute('type')) {
+            item.children[1].removeAttribute('type')
+        } else {
+            item.children[1].setAttribute('type', 'password')
+        }
+    })
+});
 
 
 
+// Tabla Estado Civil - onchange
 
+const insertarFilas = document.querySelector('.tabla-estado-civil')
 
+function cantidadFamilia(cantidad) {
 
+    if (cantidad != 0) {
+        let resultado =
+            `<div class="estCivil-row">
+                <div class="estCivil-nombre">Nombre y Apellido</div>
 
+                <div class="estCivil-cuil">Cuil</div>
+
+                <div class="estCivil edad">Edad</div>
+
+                <div class="estCivil-parentesco">Parentesco</div>
+            </div>`
+
+        for (let i = 0; i < cantidad ; i++) {
+            resultado +=
+                `<div class="estCivil-row">
+                    <div class="estCivil-nombre">
+                        <input type="text">
+                    </div>
+
+                    <div class="estCivil-cuil">
+                        <input type="text">
+                    </div>
+
+                    <div class="estCivil edad">
+                        <input type="text">
+                    </div>
+
+                    <div class="estCivil-parentesco">
+                        <div class="select-input">
+                            <div class="sel-icons">
+                                &#x22c1;
+                            </div>
+
+                            <div class="tap-blan"></div>
+
+                            <select name="" id="">
+                                <option value="Nacionalidad">Parentesco</option>
+                                <option value="Padre">Padre</option>
+                                <option value="Madre">Madre</option>
+                                <option value="Hermano">Hermano</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>`
+        }
+
+        insertarFilas.innerHTML = resultado
+
+    } else {
+        insertarFilas.innerHTML = ''
+        
+    }
+
+}
 
 
 
